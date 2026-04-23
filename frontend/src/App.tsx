241c32fbc42f6067d3e4f7ca7ed555b59aac5fc1
ilyoil2 <0211ilyoil@gmail.com>
2026-04-23 14:08:58 +0900

    refactor delete unnecessary files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import KanjiDashboard from "./app/page"
+import KanjiDashboard from "./components/kanji-dashboard"
+import { ThemeProvider } from "./components/theme-provider"
+import { Toaster } from "./components/ui/sonner"
 
 function App() {
-  return <KanjiDashboard />
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="kanjify-theme">
+      <KanjiDashboard />
+      <Toaster position="bottom-right" richColors />
+    </ThemeProvider>
+  )
 }
 
 export default App
